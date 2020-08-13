e2d1ade3d5742a9ce4a80debde47155285ec7f83
qizhuqing <1144577212@qq.com>
2020-08-14 00:14:44 +0800

    update 20200814

--- src/main/java/com/qzq/leetcode/hashmap/HashMap.java
+++ src/main/java/com/qzq/leetcode/hashmap/HashMap.java
@@ -1,5 +1,44 @@
 package com.qzq.leetcode.hashmap;
 
-public class HashMap {
+import java.io.IOException;
+import java.io.InvalidObjectException;
+import java.io.Serializable;
+import java.lang.reflect.ParameterizedType;
+import java.lang.reflect.Type;
+import java.util.AbstractCollection;
+import java.util.AbstractMap;
+import java.util.AbstractSet;
+import java.util.Collection;
+import java.util.ConcurrentModificationException;
+
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.Map;
+import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.Set;
+import java.util.Spliterator;
+
+import java.util.function.BiConsumer;
+import java.util.function.BiFunction;
+import java.util.function.Consumer;
+import java.util.function.Function;
+
+
+public class HashMap<K,V> extends AbstractMap<K,V>
+    implements Map<K,V>, Cloneable, Serializable {
+
+	@Override
+	public Set<Entry<K, V>> entrySet() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+
+
+
+
+   
 
 }
